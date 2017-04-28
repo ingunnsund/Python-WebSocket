3828b59fa093ee7fcbd1f12d326480e246ac6da5
Knut Kirkhorn <knutkirk@hotmail.com>
2017-04-28 16:32:11 +0200

    Added structure and files to project.

---
+++ websocket_lib/websocket.py
@@ -0,0 +1,15 @@
+class WebSocket(object):
+
+    GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+
+    def __init__(self, port_number=80):
+        self.port_number = port_number
+        #WSS
+        #PROXY
+        #THREADS
+
+    def do_handshake(self):
+        print("HANDSHAKE")
+
+    def send_message(self):
+        print("SEND_MESSAGE")
