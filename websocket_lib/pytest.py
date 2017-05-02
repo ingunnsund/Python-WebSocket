3bc50f950df69eaf4b7c2b169169596c41d90abe
ingunnsund <ingunnsu@online.no>
2017-05-02 17:46:35 +0200

    Added exception + testing

--- websocket_lib/pytest.py
+++ websocket_lib/pytest.py
@@ -4,7 +4,7 @@ print("test")
 
 from websocket_lib.frames import Frames
 
-frames = Frames()
+frames = Frames(16777215)
 #test, opcode = frames.decode_message(b"\x8a\x85\x37\xfa\x21\x3d\x7f\x9f\x4d\x51\x58") #(b'\x81\x83!\xba+Ai\xdfB')
 print("pong-frame:")
 #print(test)
@@ -13,7 +13,8 @@ print("pong-frame:")
 
 #test2, test3 = frames.decode_message(b'\x81\x05Hello')
 
-result = frames.encode_frame(Opcode.TEXT_FRAME, "Hello")
+result = frames.encode_frame(Opcode.PING_FRAME, "")
 print(result)
+"""print(result)
 message=""
-print(bytes(message, "ascii"))
+print(bytes(message, "ascii"))"""
